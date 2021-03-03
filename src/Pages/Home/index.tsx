64cdbea36c9fa7d3ac8368ce9e1f067f9910a9cf
pedrorossato <phrossato@inf.ufsm.br>
2021-03-03 09:38:06 -0300

    adicionado hello world, logo da ufsm e mudado nome do site

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,7 +3,11 @@ import React from 'react';
 // import { Container } from './styles';
 
 const Home: React.FC = () => {
-  return <div />;
+  return (
+    <div>
+      <h1>Site do planetario ufsm</h1>
+    </div>
+  );
 };
 
 export default Home;
