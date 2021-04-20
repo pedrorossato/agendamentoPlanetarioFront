43bc42f8316e7f4625f07a468f9992ad685addfa
pedrorossato <62031970+pedro-rossato@users.noreply.github.com>
2021-04-20 20:00:46 -0300

    Adicionado loading spinner

--- src/Services/ApiService.ts
+++ src/Services/ApiService.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: 'https://planetarioufsm.herokuapp.com/'
-  baseURL: 'https://localhost:5001'
+  baseURL: 'https://planetarioufsm.herokuapp.com/'
+  // baseURL: 'https://localhost:5001'
   // headers: {"Access-Control-Allow-Origin": "*"}
 });
 
